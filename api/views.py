430649b5836825467770ddb218ae17eb2140344e
oduduabasi <yakndarausoro1@gmail.com>
2025-04-24 16:16:43 +0100

    i changed the display message

--- api/views.py
+++ api/views.py
@@ -3,4 +3,4 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def hello_world(request):
-    return Response({"message": "Hello World!"})
+    return Response({"message": "I Escaped Tutorial Hell!"})
